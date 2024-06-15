6b288064f42c9c3dd5c28c80314d1f95734662e2
Higor Carlos <higor.carlos6@gmail.com>
2024-06-14 22:02:41 -0300

    Configurando variaveis de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST', 'localhost'),
+        port: Number(configService.get('DB_PORT', 5432)),
+        username: configService.get('DB_USER', 'postgres'),
+        password: configService.get('DB_PASSWORD', '123'),
+        database: configService.get('DB_DBs', 'desafio_back_end'),
+        entities: [],
+        synchronize: true,
+      })
+    }),
+  ],
   controllers: [],
   providers: [],
 })
